821fed65444630b44052a4284f6567144f9b3c89
arqanus <xjhonathanx@gmail.com>
2023-03-10 16:12:01 -0500

    video 9 curso gas avanzado

--- funciones.js
+++ funciones.js
@@ -5,3 +5,10 @@ function doGet(){
 function obtenerDatosHtml(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent()
 }
+
+
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1SvPGq6QnhtDJ5L6czm9GmWJPfgzmvDf60ju_ARENrEc').getActiveSheet()
+    let datos = hoja.getDataRange().getValues()
+    return datos
+}
