c69be5e5f044340e6eb5062e4557f04241202bf1
arqanus <xjhonathanx@gmail.com>
2023-03-12 00:57:28 -0500

    video 15 curso gas avanzado

--- funciones.js
+++ funciones.js
@@ -1,6 +1,16 @@
-function doGet(){
+const HOJA = SpreadsheetApp.openById('1SvPGq6QnhtDJ5L6czm9GmWJPfgzmvDf60ju_ARENrEc').getActiveSheet()   
+
+
+function doGet(datos){
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Prueba_jlcm')
 }
+
+
+function doPost(datos){
+   return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Prueba_jlcm')
+
+}
+
 
 function obtenerDatosHtml(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent()
@@ -8,7 +18,11 @@ function obtenerDatosHtml(nombre){
 
 
 function obtenerContactos(){
-    let hoja = SpreadsheetApp.openById('1SvPGq6QnhtDJ5L6czm9GmWJPfgzmvDf60ju_ARENrEc').getActiveSheet()
-    let datos = hoja.getDataRange().getValues()
-    return datos
+    return HOJA.getDataRange().getValues()
+}
+
+function insertarContacto(nombre, correo) {
+
+    HOJA.appendRow([nombre, correo])
+    
 }
